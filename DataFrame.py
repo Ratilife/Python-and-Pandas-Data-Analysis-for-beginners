6259807d9983d2049bbe1a8633525b35fc80d881
Ratilife <ratilife81@gmail.com>
2025-01-19 17:22:45 +0300

    простой пример

--- DataFrame.py
+++ DataFrame.py
@@ -1,1 +1,8 @@
 #DataFrame - таблицы. Состоит из Series
+from pandas import DataFrame
+df = DataFrame(index=['USA', 'Russia', 'China'],
+               data={
+                   'gdp': [28781, 2056, 18532],
+                   'population': [335.8, 146.1, 1409.6],
+                   'region': ['America', 'Europe', 'Asia']
+               })
