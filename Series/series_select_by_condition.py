fcfbe89e06d305eafe0500efe0f94b92c177ecc3
Ratilife <ratilife81@gmail.com>
2025-01-19 18:04:26 +0300

    добавлен пример

---
+++ Series/series_select_by_condition.py
@@ -0,0 +1,34 @@
+from pandas import Series
+
+"""
+    Select elements from series by condition
+"""
+
+s = Series(data={
+    'USA': 28781,
+    'Russia': 2056,
+    'China': 18532,
+    'South Korea': 1760,
+    'France': 3130,
+    'Germany': 4591,
+    'UK': 3495,
+    'Japan': 4110,
+    'Canada': 2242
+})
+
+# маска/состояние
+condition = s > 10000
+assert isinstance(condition, Series)
+print(s[condition])
+
+# сложное состояние
+print('Countries with GDP between 2 and 20 trln USD:')
+print(s[(s > 2000) & (s < 20000)])
+
+# использование НЕ в условии
+print('Other countries: use ~ operator')
+print(s[~((s > 2000) & (s < 20000))])
+
+# выбрать по числовому индексу: используйте iloc
+print('Select by numeric index')
+print(s.iloc[1:3])
